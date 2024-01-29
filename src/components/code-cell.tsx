9039212cb51b99648b4ca69c073bc58cc5ef2387
Andrew Learmouth <andrewlearmouth@Tests-MacBook-Pro.local>
2024-01-29 10:37:55 +0000

    Implemented horizontal sizing and used stateto keep track of the size of the window.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,24 +1,30 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
 import bundle from '../bundler';
 import Resizable from './resizable';
 
 const CodeCell = () => {
-  const [input, setInput] = useState('');
   const [code, setCode] = useState('');
+  const [input, setInput] = useState('');
 
-  const onClick = async () => {
-    const output = await bundle(input);
-    setCode(output);
-  };
+  useEffect(() => {
+    const timer = setTimeout(async () => {
+      const output = await bundle(input);
+      setCode(output);
+    }, 750);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [input]);
 
   return (
     <Resizable direction="vertical">
       <div style={{ height: '100%', display: 'flex', flexDirection: 'row' }}>
         <Resizable direction="horizontal">
           <CodeEditor
-            initialValue="const a = 1"
+            initialValue="const a = 1;"
             onChange={(value) => setInput(value)}
           />
         </Resizable>
@@ -27,4 +33,5 @@ const CodeCell = () => {
     </Resizable>
   );
 };
+
 export default CodeCell;
