39c9362869d24ba6150d287f294ac46f7a2ecf2f
Andrew Learmouth <andrewlearmouth@Tests-MacBook-Pro.local>
2024-01-30 17:08:11 +0000

    Added a progress bar and styling to show that bar depending on the load time. I have also put a white background behind the preview component so it remains white at all times rather than showing the blue background when it is loading.

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -1,3 +1,4 @@
+import './code-cell.css';
 import { useEffect } from 'react';
 import CodeEditor from './code-editor';
 import Preview from './preview';
@@ -16,6 +17,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
   const bundle = useTypedSelector((state) => state.bundles[cell.id]);
 
   useEffect(() => {
+    if (!bundle) {
+      createBundle(cell.id, cell.content);
+      return;
+    }
     const timer = setTimeout(async () => {
       createBundle(cell.id, cell.content);
     }, 750);
@@ -23,6 +28,8 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     return () => {
       clearTimeout(timer);
     };
+
+    //eslint-disable-next-line react-hooks/exhaustive-deps
   }, [cell.content, cell.id, createBundle]);
 
   return (
@@ -40,7 +47,19 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             onChange={(value) => updateCell(cell.id, value)}
           />
         </Resizable>
-        {bundle && <Preview code={bundle.code} err={bundle.err} />}
+        <div className="progress-wrapper">
+          {!bundle || bundle.loading ? (
+            <div>
+              <div className="progress-cover">
+                <progress className="progress is-small is-primary" max="100">
+                  Loading
+                </progress>
+              </div>
+            </div>
+          ) : (
+            <Preview code={bundle.code} err={bundle.err} />
+          )}
+        </div>
       </div>
     </Resizable>
   );
